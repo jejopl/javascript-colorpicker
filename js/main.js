5d3991f10128dfde54c0e06e25725c414585cb00
unknown <jejopl@o2.pl>
2019-04-15 11:24:56 +0200

    cleanup the code

--- js/main.js
+++ js/main.js
@@ -10,13 +10,10 @@ const hex = document.getElementById('hex');
 const rgb = document.getElementById('rgb');
 
 function rgbaToRgb(r, g, b, a) {
-
     let rR, rG, rB 
-
     rR = ((1 - a) * 255) + (a * r)
     rG = ((1 - a) * 255) + (a * g)
     rB = ((1 - a) * 255) + (a * b)
-
     rR = (rR < 255) ? Math.round(rR) : 255
     rG = (rG < 255) ? Math.round(rG) : 255
     rB = (rB < 255) ? Math.round(rB) : 255
@@ -37,8 +34,6 @@ function fullColorHex(r,g,b) {
     let blue = rgbToHex(b);
     return ('#' + red+green+blue).toUpperCase();
 };
-
-
 
 const startPos = {
     x: 125,
@@ -61,9 +56,7 @@ const sideSwitchPos = {
 
 let drag, sideDrag, dragEnd, sideY
 
-
-// side layer color
-//const someColors = ['red', 'orange', 'yellow', 'chartreuse', 'green', 'aqua', 'blue', 'violet', 'mistyrose'];
+// draw side layer gradient
 const someColors = ['#FF0000', '#FFFF00', '#00FF00', '#00FFFF', '#0000FF', '#FF00FF',]
 let outer_layer1_gr = outer_layer1_ctx.createLinearGradient(0, 0, 0, 250);
 for (i = 0; i < someColors.length; i++) {
@@ -80,36 +73,42 @@ outer_layer2_ctx.strokeStyle = '#000';
 outer_layer2_ctx.lineWidth = 2
 outer_layer2_ctx.rect(5, 1, 30, 10)
 outer_layer2_ctx.stroke();
-//outer_layer2_ctx.fill();
-
 
 // layer1 (color)
-let layer1_gr = layer1_ctx.createLinearGradient(0, 0, 250, 0);
-layer1_gr.addColorStop(1, `#ff0000`);
-layer1_gr.addColorStop(.95, `#ff0000`);
-layer1_gr.addColorStop(0, `#fff`);
-layer1_ctx.fillStyle = layer1_gr;
-layer1_ctx.fillRect(0, 0, layer1_ctx.canvas.width, layer1_ctx.canvas.height);
+function drawColorLayer(color = `#ff0000`) {
+    let layer1_gr = layer1_ctx.createLinearGradient(0, 0, 250, 0);
+    layer1_gr.addColorStop(1, color);
+    layer1_gr.addColorStop(.95, color);
+    layer1_gr.addColorStop(0, `#fff`);
+    layer1_ctx.fillStyle = layer1_gr;
+    layer1_ctx.fillRect(0, 0, layer1_ctx.canvas.width, layer1_ctx.canvas.height);
+}
 
 // layer1 (black n white)
-let layer1_gr2 = layer1_ctx.createLinearGradient(0, 0, 0, 250);
-layer1_gr2.addColorStop(1, "rgba(0,0,0,1)");
-layer1_gr2.addColorStop(.95, "rgba(0,0,0,1)");
-layer1_gr2.addColorStop(.5, "rgba(0,0,0,0.2)");
-layer1_gr2.addColorStop(.4, "rgba(0,0,0,0.1)");
-layer1_gr2.addColorStop(.1, "rgba(0,0,0,0)");
-layer1_ctx.fillStyle = layer1_gr2;
-layer1_ctx.fillRect(0, 0, layer1_ctx.canvas.width, layer1_ctx.canvas.height);
+function drawBlackNWhiteLayer() {
+    let layer1_gr2 = layer1_ctx.createLinearGradient(0, 0, 0, 250);
+    layer1_gr2.addColorStop(1, "rgba(0,0,0,1)");
+    layer1_gr2.addColorStop(.95, "rgba(0,0,0,1)");
+    layer1_gr2.addColorStop(.5, "rgba(0,0,0,0.2)");
+    layer1_gr2.addColorStop(.4, "rgba(0,0,0,0.1)");
+    layer1_gr2.addColorStop(.1, "rgba(0,0,0,0)");
+    layer1_ctx.fillStyle = layer1_gr2;
+    layer1_ctx.fillRect(0, 0, layer1_ctx.canvas.width, layer1_ctx.canvas.height);
+}
 
+// draw bg layers (color, black and white gradient)
+drawColorLayer()
+drawBlackNWhiteLayer()
+
+// set current color on main and side layers
 let currentColor = layer1_ctx.getImageData(mainSwitchPos.x, mainSwitchPos.y, 1, 1).data;
 let alpha = (currentColor[3] / 255).toFixed(2)
-
 currentColor = rgbaToRgb(currentColor[0], currentColor[1], currentColor[2], alpha)
-
 let sideColor = outer_layer1_ctx.getImageData(sideSwitchPos.x, sideSwitchPos.y, 1, 1).data;
 let sideAlpha = (sideColor[3] / 255).toFixed(2)
-
 sideColor = rgbaToRgb(sideColor[0], sideColor[1], sideColor[2], sideAlpha)
+
+// display color values on the page
 rgb.value = `rgb(${currentColor[0]}, ${currentColor[1]}, ${currentColor[2]})`
 hex.value = fullColorHex(currentColor[0], currentColor[1], currentColor[2])
 
@@ -124,7 +123,7 @@ function isMouseOnSwitch(layerX, layerY, switchPosX, switchPosY, switchSizeX, sw
     } else false
 }
 
-// draw function for picker (circle)
+// draw function for the main switch
 function draw() {
     layer2_ctx.beginPath();
     layer2_ctx.arc(140, 140, 8, 0, 2 * Math.PI);
@@ -135,6 +134,9 @@ function draw() {
     layer2_ctx.stroke();
 }
 
+draw()
+
+// clear function for the main switch
 function clear() {
     layer2_ctx.save();
     layer2_ctx.setTransform(1, 0, 0, 1, 0, 0);
@@ -142,17 +144,13 @@ function clear() {
     layer2_ctx.restore();
 }
 
-draw()
-
 layer2.addEventListener('mousedown', e => {
     if (isMouseOnSwitch(e.layerX, e.layerY, mainSwitchPos.x + 5, mainSwitchPos.y + 5, 18, 18)) {
         dragStart = {
             x: e.layerX,
             y: e.layerY
         }
-
         drag = true;
-
     }
 });
 
@@ -168,6 +166,7 @@ layer2.addEventListener('mousemove', e => {
             y: e.layerY
         };
 
+        // set translate to 0 if switch is on max top/bottom/left/right (block switch to move out of range)
         mainY = ((mainSwitchPos.y < 2 && dragEnd.y - dragStart.y < 0) || (mainSwitchPos.y > 248 && dragEnd.y - dragStart.y > 0)) ? 0 : dragEnd.y - dragStart.y
         mainX = ((mainSwitchPos.x < 2 && dragEnd.x - dragStart.x < 0) || (mainSwitchPos.x > 248 && dragEnd.x - dragStart.x > 0)) ? 0 : dragEnd.x - dragStart.x
 
@@ -186,6 +185,8 @@ layer2.addEventListener('mousemove', e => {
         result[0].style = `fill: rgb(${currentColor[0]}, ${currentColor[1]}, ${currentColor[2]})`
         result[1].style = `fill: rgb(${currentColor[0]}, ${currentColor[1]}, ${currentColor[2]})`
         result[2].style = `fill: rgb(${currentColor[0]}, ${currentColor[1]}, ${currentColor[2]})`
+
+        // display color values on the page
         hex.value = fullColorHex(currentColor[0], currentColor[1], currentColor[2])
         rgb.value = `rgb(${currentColor[0]}, ${currentColor[1]}, ${currentColor[2]})`
     }
@@ -198,14 +199,12 @@ layer2.addEventListener('mouseup', e => {
         y: e.layerY
     }
 
+    // set translate to 0 if switch is on max top/bottom/left/right (block switch to move out of range)
     mainY = ((mainSwitchPos.y < 2 && dragEnd.y - dragStart.y < 0) || (mainSwitchPos.y > 248 && dragEnd.y - dragStart.y > 0)) ? 0 : dragEnd.y - dragStart.y
     mainX = ((mainSwitchPos.x < 2 && dragEnd.x - dragStart.x < 0) || (mainSwitchPos.x > 248 && dragEnd.x - dragStart.x > 0)) ? 0 : dragEnd.x - dragStart.x
 
     startPos.x += mainX
     startPos.y += mainY
-
-    // startPos.x += (dragEnd.x - dragStart.x);
-    // startPos.y += (dragEnd.y - dragStart.y);
     mainSwitchPos.x = startPos.x;
     mainSwitchPos.y = startPos.y;
     layer2_ctx.translate(mainX, mainY);
@@ -220,9 +219,10 @@ layer2.addEventListener('mouseup', e => {
     result[0].style = `fill: rgb(${currentColor[0]}, ${currentColor[1]}, ${currentColor[2]})`
     result[1].style = `fill: rgb(${currentColor[0]}, ${currentColor[1]}, ${currentColor[2]})`
     result[2].style = `fill: rgb(${currentColor[0]}, ${currentColor[1]}, ${currentColor[2]})`
+
+    // display color values on the page
     hex.value = fullColorHex(currentColor[0], currentColor[1], currentColor[2])
     rgb.value = `rgb(${currentColor[0]}, ${currentColor[1]}, ${currentColor[2]})`
-
 });
 
 
@@ -230,8 +230,6 @@ outer_layer2.addEventListener('mousemove', e => {
 
     if (isMouseOnSwitch(e.layerX, e.layerY, sideSwitchPos.x, sideSwitchPos.y, 34, 14)) outer_layer2.style.cursor = 'pointer'
     else outer_layer2.style.cursor = 'default'
-    
-    
 
     if (sideDrag) {
         sideDragEnd = {
@@ -239,41 +237,28 @@ outer_layer2.addEventListener('mousemove', e => {
             y: e.layerY
         };
 
-        // set translate to 0 if switch is on max top or max bottom (block switch to move out of range)
+        // set translate to 0 if switch is on max top/bottom (block switch to move out of range)
         sideY = ((sideSwitchPos.y < 2 && sideDragEnd.y - sideDragStart.y < 0) || (sideSwitchPos.y > 248 && sideDragEnd.y - sideDragStart.y > 0)) ? 0 : sideDragEnd.y - sideDragStart.y
         sideSwitchPos.y += sideY;
 
+        // move the switch (translate), clear and draw again
         outer_layer2_ctx.translate(0, sideY);
         outer_layer2_ctx.clearRect(0, 0, outer_layer2.width, outer_layer2.height);
         outer_layer2_ctx.clearRect(0, 0, outer_layer2.width, -outer_layer2.height);
-        
         outer_layer2_ctx.beginPath()
         outer_layer2_ctx.rect(5, 2, 30, 10)
         outer_layer2_ctx.stroke();
 
         sideColor = outer_layer1_ctx.getImageData(sideSwitchPos.x, sideSwitchPos.y, 1, 1).data;
         sideDragStart = sideDragEnd;
-        layer1_ctx.clearRect(0, 0, layer1.width, layer1.height);
 
-        layer1_gr = layer1_ctx.createLinearGradient(0, 0, 250, 0);
-        layer1_gr.addColorStop(1, `rgba(${sideColor[0]},${sideColor[1]},${sideColor[2]}, ${sideAlpha})`);
-        layer1_gr.addColorStop(.95, `rgba(${sideColor[0]},${sideColor[1]},${sideColor[2]}, ${sideAlpha})`);
-        layer1_gr.addColorStop(0, `rgba(${sideColor[0]},${sideColor[1]},${sideColor[2]}, 0)`);
-        layer1_ctx.fillStyle = layer1_gr;
-        layer1_ctx.fillRect(0, 0, layer1_ctx.canvas.width, layer1_ctx.canvas.height);
+        // draw bg layers (color, black and white gradient)
+        drawColorLayer(`rgba(${sideColor[0]},${sideColor[1]},${sideColor[2]}, ${sideAlpha})`)
+        drawBlackNWhiteLayer()
 
-        layer1_gr2 = layer1_ctx.createLinearGradient(0, 0, 0, 250);
-        layer1_gr2.addColorStop(1, "rgba(0,0,0,1)");
-        layer1_gr2.addColorStop(.95, "rgba(0,0,0,1)");
-        layer1_gr2.addColorStop(.5, "rgba(0,0,0,0.2)");
-        layer1_gr2.addColorStop(.4, "rgba(0,0,0,0.1)");
-        layer1_gr2.addColorStop(.1, "rgba(0,0,0,0)");
-        layer1_ctx.fillStyle = layer1_gr2;
-        layer1_ctx.fillRect(0, 0, layer1_ctx.canvas.width, layer1_ctx.canvas.height);
-
+        // save current color value from main switch position
         currentColor = layer1_ctx.getImageData(mainSwitchPos.x, mainSwitchPos.y, 1, 1).data;
         alpha = (currentColor[3] / 255).toFixed(2)
-
         currentColor = rgbaToRgb(currentColor[0], currentColor[1], currentColor[2], alpha)
 
         result[0].style = `fill: rgb(${currentColor[0]}, ${currentColor[1]}, ${currentColor[2]})`
@@ -293,19 +278,15 @@ outer_layer2.addEventListener('mousedown', e => {
             x: e.layerX,
             y: e.layerY
         }
-
         sideDrag = true;
-
     }
 });
 
 outer_layer2.addEventListener('mouseup', e => {
-
-        sideDrag = false;
-
+    sideDrag = false;
 });
 
-
+// copy the value of the color (rgb or hex) after mouse click
 [hex,rgb].forEach(item => {
     item.addEventListener('mouseover', e => {
         item.style.cursor='pointer';
